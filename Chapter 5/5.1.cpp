67139164892394072ecc461500f836a1276dff45
mlsulekhiya <44512130+mlsulekhiya@users.noreply.github.com>
2019-10-12 22:54:16 +0530

    included main function.

--- Chapter 5/5.1.cpp
+++ Chapter 5/5.1.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 
 int setBit(int N, int M, int i, int j){
-	int max = ~0;
+	int max = 0;
 	int left = max - ((1<<j)-1);
 	int right = (1 << i - 1);
 	int  mask = right | left;
-	return (n & mask) | (m << i);
+	return (n & mask) | (m << i)
 	int  mask = right | left;
 }
 
-
+int main(){
+	
+return 0;	
+}
